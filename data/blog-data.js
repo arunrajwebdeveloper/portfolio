362a1fe8f5b3a6ff730edeaa4265da9bda18157a
arunraj.r <arunraj.r@nergyindia.com>
2025-06-04 18:46:04 +0530

    blogs

---
+++ data/blog-data.js
@@ -0,0 +1,99 @@
+const blogData = {
+  blog: [
+    {
+      id: "1",
+      title:
+        "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.",
+      description:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+      thumbnail: "./data/blog-images/dropdown-image.png",
+      body: [
+        {
+          title: "HTML",
+          description: "This is html",
+          code: {
+            language: "markup",
+            content: `<div class="container">
+  <h1>Hello, World!</h1>
+</div>`,
+          },
+          image: "",
+        },
+        {
+          title: "CSS",
+          description: "Hiii",
+          code: {
+            language: "css",
+            content: `.container {
+  padding: 20px;
+  background-color: #f0f0f0;
+  color: #333;
+}`,
+          },
+          image: "",
+        },
+        {
+          title: "JavaScript",
+          description: "hellaaa",
+          code: {
+            language: "javascript",
+            content: `function greet(name) {
+  console.log(\`Hello, \${name}!\`);
+}
+greet("VS Code");`,
+          },
+          image: "",
+        },
+      ],
+
+      tags: ["Html", "Css", "Javascript"],
+    },
+    {
+      id: "2",
+      title:
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      description:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.",
+      thumbnail: "./data/blog-images/code-snapshot.png",
+      body: [
+        {
+          title: "HTML",
+          description: "This is html",
+          code: {
+            language: "markup",
+            content: `<div class="container">
+  <h1>Hello, World!</h1>
+</div>`,
+          },
+          image: "",
+        },
+        {
+          title: "CSS",
+          description: "Hiii",
+          code: {
+            language: "css",
+            content: `.container {
+  padding: 20px;
+  background-color: #f0f0f0;
+  color: #333;
+}`,
+          },
+          image: "",
+        },
+        {
+          title: "JavaScript",
+          description: "hellaaa",
+          code: {
+            language: "javascript",
+            content: `function greet(name) {
+  console.log(\`Hello, \${name}!\`);
+}
+greet("VS Code");`,
+          },
+          image: "",
+        },
+      ],
+      tags: ["React", "Ui/Ux", "Typescript", "SocketIo"],
+    },
+  ],
+};
