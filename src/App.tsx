c06ce9f4c1ec730d6fd6a3e5cb604d7aee111845
arunraj.r <arunraj.r@nergyindia.com>
2025-11-10 12:49:25 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ function App() {
         <h1 className="text-black font-display font-semibold text-9xl">
           Building modern, responsive web experiences with passion.
         </h1>
-        <p className="mt-4 text-gray-700 text-6xl">
+        <p className="mt-4 text-slate-700 text-6xl">
           I’m <span className="font-sans">Arun Raj</span>, by the way — thanks
           for visiting my portfolio!
         </p>
       </div>
       <div className="flex max-w-6xl flex-col justify-center min-h-screen mx-auto space-y-8">
-        <p className="mt-4 text-gray-700 text-7xl">
+        <p className="mt-4 text-slate-700 text-7xl">
           My main focus is creating modern, responsive Frontend applications
           with ReactJS — but I also enjoy developing scalable APIs with NestJS.
         </p>
