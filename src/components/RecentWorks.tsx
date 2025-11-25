99606359841ecf72a61055b657b533b9f8a8e2b9
arunraj.r <arunraj.r@nergyindia.com>
2025-11-25 13:21:14 +0530

    works

--- src/components/RecentWorks.tsx
+++ src/components/RecentWorks.tsx
@@ -31,7 +31,8 @@ function RecentWorks() {
     <section className="flex flex-col justify-center bg-white w-full py-20 min-h-screen">
       <div className="relative">
         <h2 className="text-2xl text-center">Recent Works</h2>
-        <div className="grid md:grid-cols-2 gap-x-48 gap-y-24 align-middle  relative mt-32">
+        <div className=" flex flex-col gap-16 relative mt-32">
+          <div className="w-px bg-[#D1D1D1] absolute left-1/2 top-0 bottom-0 z-0"></div>
           {items.map((item, index) => {
             const isRight = index % 2 !== 0;
 
@@ -39,34 +40,38 @@ function RecentWorks() {
               <div
                 key={index}
                 className={`relative flex w-full ${
-                  isRight
-                    ? "md:col-start-2 mt-48 justify-start"
-                    : "md:col-start-1 justify-end"
-                }`}
+                  index === 0 ? "mt-0" : "-mt-[400px]"
+                } ${isRight ? "justify-start" : "justify-end"}`}
                 style={{ height: ITEM_HEIGHT }}
               >
-                <div className="w-full relative">
+                {index === 0 && (
+                  <div className="absolute left-0 text-[16rem] tracking-tight w-[calc(50%-100px)] flex justify-center items-center">
+                    W.
+                  </div>
+                )}
+
+                <div className="w-[calc(50%-100px)] relative">
                   <ParallaxImage
                     src={item.src}
                     speed={40}
                     height={ITEM_HEIGHT}
                   />
-                </div>
 
-                <motion.div
-                  // initial={{ opacity: 0, y: 100 }}
-                  // whileInView={{ opacity: 1, y: 0 }}
-                  // transition={{ duration: 0.6, ease: [0.33, 1, 0.68, 1] }}
-                  // viewport={{ once: false, amount: 0.4 }}
-                  className={`absolute top-1/2 -translate-y-1/2 w-auto ${
-                    isRight
-                      ? " -ml-16 text-left" // push text to outside left of right image
-                      : " -mr-16 text-right" // push text to outside right of left image
-                  }`}
-                >
-                  <h3 className="text-3xl text-black mb-2">{item.title}</h3>
-                  <p className="text-black leading-relaxed">{item.text}</p>
-                </motion.div>
+                  <motion.div
+                    // initial={{ opacity: 0, y: 100 }}
+                    // whileInView={{ opacity: 1, y: 0 }}
+                    // transition={{ duration: 0.6, ease: [0.33, 1, 0.68, 1] }}
+                    // viewport={{ once: false, amount: 0.4 }}
+                    className={`absolute top-1/2 -translate-y-1/2 w-[360px] ${
+                      isRight
+                        ? "right-0 translate-x-1/4 text-right"
+                        : "left-0 -translate-x-1/4 text-left"
+                    }`}
+                  >
+                    <h3 className="text-3xl text-black mb-2">{item.title}</h3>
+                    <p className="text-black leading-relaxed">{item.text}</p>
+                  </motion.div>
+                </div>
               </div>
             );
           })}
