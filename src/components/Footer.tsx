5aafb3694f257c652c62ca532de29ce574824ec5
arunraj.r <arunraj.r@nergyindia.com>
2025-11-26 17:54:07 +0530

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,11 @@ export default function Footer() {
       <div className="fixed bottom-0 h-[800px] w-full flex">
         <div className="max-w-4xl mx-auto h-full flex flex-col justify-between px-4 py-28">
           <div className="text-center flex items-center flex-col">
-            <p className="m-auto text-9xl text-white mb-14">
+            <p className="m-auto text-9xl text-white mb-14 tracking-wider">
               Let's work togather
             </p>
             <a
-              className="text-white rounded-4xl border-2 border-white px-6 py-4 max-w-48 flex gap-x-2 items-center tracking-wider outline-0"
+              className="text-white group rounded-4xl border-2 border-white hover:text-[#ffd53e] px-6 py-4 max-w-48 flex gap-x-2 items-center tracking-wider outline-0 hover:border-[#ffd53e] transition duration-300"
               href="mailto:arunrajwebdeveloper@gmail.com"
             >
               <span>Say Hello</span>
@@ -30,6 +30,7 @@ export default function Footer() {
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
+                    className="group-hover:stroke-[#ffd53e] stroke-[#ffffff] transition duration-300"
                   />
                 </g>
               </svg>
@@ -37,11 +38,13 @@ export default function Footer() {
           </div>
           <div className="flex justify-between items-end">
             <div>
-              <p className="text-gray-400 text-sm mb-3">Social</p>
+              <p className="text-gray-400 text-sm mb-3 tracking-wider">
+                Social
+              </p>
               <ul className="flex list-none gap-6">
                 <li className="text-white text-base">
                   <a
-                    className="text-white no-underline"
+                    className="text-white no-underline tracking-wider hover:text-[#ffd53e] transition duration-300"
                     href=""
                     target="_blank"
                   >
@@ -50,7 +53,7 @@ export default function Footer() {
                 </li>
                 <li className="text-white text-base">
                   <a
-                    className="text-white no-underline"
+                    className="text-white no-underline tracking-wider hover:text-[#ffd53e] transition duration-300"
                     href=""
                     target="_blank"
                   >
@@ -61,10 +64,10 @@ export default function Footer() {
             </div>
 
             <div>
-              <p className="text-white text-sm">
+              <p className="text-white text-sm tracking-wider">
                 Designed, coded, and shipped by me.
               </p>
-              <p className="text-white text-sm">
+              <p className="text-white text-sm tracking-wider">
                 <span>&copy;</span>
                 {`${new Date().getFullYear()} Arunraj`}
               </p>
