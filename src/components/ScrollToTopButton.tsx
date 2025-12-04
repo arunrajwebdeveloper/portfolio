e70afae86da76fc36a5e9824e06782380d44bef4
arunraj.r <arunraj.r@nergyindia.com>
2025-12-04 13:25:08 +0530

    anim

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -1,4 +1,43 @@
+import { motion, useScroll, type Variants } from "framer-motion";
+import { useEffect, useState } from "react";
+
 const ScrollToTopButton = () => {
+  const { scrollY } = useScroll();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const indicatorVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 100,
+      transition: { duration: 0.3 },
+    },
+    visible: {
+      opacity: 1,
+      y: 0, // Moves to its final position (0 offset)
+      transition: { duration: 0.5, type: "spring", stiffness: 100 },
+    },
+  };
+
+  useEffect(() => {
+    // Function to update the visibility state based on scrollY position
+    const updateVisibility = (latestScrollY: number) => {
+      // Show the div if the scroll position is greater than 400px
+      if (latestScrollY > 600) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    // Subscribe to the scrollY MotionValue
+    const unsubscribe = scrollY.on("change", updateVisibility);
+
+    // Cleanup function to remove the subscription when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollY]);
+
   const handleScrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -7,7 +46,9 @@ const ScrollToTopButton = () => {
   };
 
   return (
-    <div
+    <motion.div
+      variants={indicatorVariants}
+      animate={isVisible ? "visible" : "hidden"}
       onClick={handleScrollToTop}
       className="fixed cursor-pointer -rotate-90 group right-2 top-1/2 -translate-y-1/2 z-50 mix-blend-difference hidden md:block"
     >
@@ -20,7 +61,7 @@ const ScrollToTopButton = () => {
       <span className="text-white text-sm uppercase whitespace-nowrap">
         Scroll Top
       </span>
-    </div>
+    </motion.div>
   );
 };
 
